3a106d18dfd7bb2b7e933ca78450989a04e369ca
AlexandreCousseauarchi <159045944+AlexandreCousseauarchi@users.noreply.github.com>
2024-02-06 11:11:57 +0100

    Create traitement-formulaire.php

---
+++ traitement-formulaire.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["Nom-2"];
+    $email = $_POST["email-2"];
+    $telephone = $_POST["T-l-phone-2"];
+    $message = $_POST["field-2"];
+    
+    // Adresse e-mail où vous souhaitez recevoir les informations soumises
+    $destinataire = "alexandrecousseau.archi@gmail.com";
+    $sujet = "Nouveau message de formulaire";
+    $contenu = "Nom: $nom\n";
+    $contenu .= "Email: $email\n";
+    $contenu .= "Téléphone: $telephone\n";
+    $contenu .= "Message:\n$message";
+    
+    // Envoi de l'e-mail
+    mail($destinataire, $sujet, $contenu);
+    
+    // Redirection après l'envoi du formulaire (facultatif)
+    header("Location: confirmation.html");
+    exit;
+}
+?>
